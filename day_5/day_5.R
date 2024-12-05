51ede703bd870fd55c1db79e81519af01d19a82c
Denisa Bogdan <42743311+denbogdan@users.noreply.github.com>
2024-12-05 13:58:45 +0000

    day 5 all

--- day_5/day_5.R
+++ day_5/day_5.R
@@ -20,13 +20,11 @@ for(x in manuals) {
   correct <- TRUE
   i <- 1
   while(correct & i < length(x)) {
-    n <- x[i]
-    
     #grab all elements after
-    rest <- x[(which(x == n)+1):length(x)]
+    rest <- x[(which(x == x[i])+1):length(x)]
     
     #grab all elements permitted after
-    permitted <- rules$after[which(rules$before == n)]
+    permitted <- rules$after[which(rules$before == x[i])]
     
     #check all there is in the rest is permitted
     correct <- (sum(rest %in% permitted) == length(rest))
@@ -36,7 +34,7 @@ for(x in manuals) {
 
   #add to sum
   if(correct) {
-    #select the middle element and add to tally 
+    #select the middle element and add to sum 
     sum <- sum + x[ceiling(length(x)/2)]
   }
 }
@@ -46,3 +44,56 @@ sum
 
 
 ##Question 2 -----------------------------------------------------------------------
+sum <- 0
+
+for(x in manuals) {
+  #check for correctness
+  correct <- TRUE
+  i <- 1
+  while(correct & i < length(x)) {
+    #grab all elements after
+    rest <- x[(which(x == x[i])+1):length(x)]
+    
+    #grab all elements permitted after
+    permitted <- rules$after[which(rules$before == x[i])]
+    
+    #check all there is in the rest is permitted
+    correct <- (sum(rest %in% permitted) == length(rest))
+    
+    i <- i+1
+  }
+  
+  #sort if incorrect
+  if(!correct) {
+    #bubble sort
+    swap_performed <- TRUE
+    
+    #loop through until they are all in order
+    while (swap_performed) {
+      swap_performed <- FALSE
+      
+      for (i in 1:(length(x) - 1)) {
+        #find the permitted neighbours
+        permitted <- rules$after[which(rules$before == x[i])]
+        
+        #check if current neighbour is permitted
+        if (!(x[i+1] %in% permitted)) {
+          #if not, swap
+          tmp <- x[i]
+          x[i] <- x[i + 1]
+          x[i + 1] <- tmp
+          
+          #continue looping until there are no swaps left to perform
+          swap_performed <- TRUE
+        }
+      }
+    }
+    
+    #select the middle element and add to sum 
+    sum <- sum + x[ceiling(length(x)/2)]
+  }
+}
+
+##Answer 2 -------------------------------------------------------------------------
+sum
+
